9444ca9876963ae1c8282395a549437c80c5d70c
kspipa <yyyyyyy5yyyy@gmail.com>
2023-10-23 23:33:00 +0300

    >>

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,18 @@ use crate::net::{start, client};
 fn main() {
     println!("Server or client?");
     println!("1 or 2 : ");
-    let mut ip = String::new();
+    let mut ip: String = String::new();
     let mut num = String::new();
     for i in std::io::stdin().lines(){ num = i.unwrap(); break;}
     let rr = num.as_str();
     if rr == "1"{
         println!("ip addr to set server : ip:port");
-        std::io::stdin().read_line(&mut ip).unwrap();
-        let v: Vec<&str> = ip.split(':').collect();
-        println!("ip : {}\nport : {}", v[0], v[1]);
-        println!("bind the server");
+        for i in std::io::stdin().lines(){ ip = i.unwrap(); break;}
         start(ip.as_str());
     }
     else if rr == "2" {
         println!("ip addr to connect : ip:port");
-        std::io::stdin().read_line(&mut ip).unwrap();
-        let v: Vec<&str> = ip.split(':').collect();
-        println!("ip : {}\nport : {}", v[0], v[1]);
+        for i in std::io::stdin().lines(){ ip = i.unwrap(); break;}
         client(ip.as_str());
     }
     else {
--- src/net.rs
+++ src/net.rs
@@ -1,9 +1,10 @@
 use std::io::{Write, Read};
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, SocketAddr};
 use std::thread;
  
 pub fn start(addr : &str) {
-    let listener = TcpListener::bind(addr).unwrap();
+    let server: SocketAddr = addr.parse().expect("Unable to parse socket address");
+    let listener = TcpListener::bind(server).unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
  
@@ -21,23 +22,26 @@ fn handle_client(mut stream: TcpStream) {
         if bytes_read == 0 {
             break;
         }
- 
-        let response = format!("Hello, {}", stream.peer_addr().unwrap().to_string());
+        println!("{}", String::from_utf8_lossy(&buffer));
+        let response = format!("Hello, {}. Your message is : {}", stream.peer_addr().unwrap().to_string(), String::from_utf8_lossy(&buffer));
         stream.write(response.as_bytes()).unwrap();
+        buffer = [0; 512];
     }
 }
  
 pub fn client(addr: &str) {
-    let mut stream = TcpStream::connect(addr).unwrap();
+    let server: SocketAddr = addr.parse().expect("Unable to parse socket address");
+    let mut stream = TcpStream::connect(server).unwrap();
     println!("successful connect");
-    let message = "Hello, server!";
+    let mut message = String::new();
     loop{
         println!("print message");
-        std::io::stdin().read_line(&mut message.to_string()).unwrap();
+        std::io::stdin().read_line(&mut message).unwrap();
         stream.write(message.as_bytes()).unwrap();
         
         let mut buffer = [0; 512];
         stream.read(&mut buffer).unwrap();
         println!("{}", String::from_utf8_lossy(&buffer));
+        message = "".to_string();
     }
 }
